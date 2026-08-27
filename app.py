03c7f11035022f0976b1bbfad12323c754ec05b1
alberto <albertojmichael@gmail.com>
2026-08-27 13:09:06 -0300

    xubi

--- app.py
+++ app.py
@@ -719,6 +719,7 @@ def aplicar_headers_de_seguranca(response):
     
     response.headers['Content-Security-Policy'] = (
         "default-src 'self'; "
+        "script-src 'self' 'unsafe-inline'; " 
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
         "font-src 'self' https://fonts.gstatic.com;"
     )
