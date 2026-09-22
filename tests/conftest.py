5ac613ea1fcfdb3ae7cd135cc40802e71917f264
alberto <albertojmichael@gmail.com>
2026-09-22 18:05:01 +0000

    jissi

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import copy
 import sys
 import os
 
+from server.state import GameState
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'server')))
 
@@ -11,17 +12,16 @@ from app import MEMORIA_SESSOES, app
 
 @pytest.fixture
 def jogo_base(mapa_mock):
-    jogo = GameState() # Ou como estiver a sua inicialização
+    jogo = GameState() 
     jogo.sala_atual = "entrada"
     
-    # ADICIONE ESTA LINHA PARA INJETAR O MAPA NO ESTADO DO JOGO:
+    
     jogo.mapa = copy.deepcopy(mapa_mock)
     
     return jogo
 
 @pytest.fixture
 def jogo_mock(mapa_mock):
-    # Faça exatamente a mesma coisa aqui, se houver uma fixture separada
     jogo = GameState()
     jogo.sala_atual = "entrada"
     jogo.mapa = copy.deepcopy(mapa_mock)
@@ -43,7 +43,7 @@ def mapa_mock():
         }
     }
 
-    
+
 @pytest.fixture
 def cliente():
     
